e13afc8fc57e6a172c0760408287a1eff1aa6ee2
szanitani <szanitani@rafal.szczecin.pl>
2016-03-05 14:40:36 +0100

    zmiana ukladu pytan, wsparcie dla local http server

--- javascripts/cowiesz.js
+++ javascripts/cowiesz.js
@@ -1,26 +1,30 @@
 jQuery(function(){
-   
+  
    jQuery.when(
       jQuery.ajax("templates/cowiesz.html"),
       jQuery.ajax("data/cowiesz.json")
    ).done(function( template, data ) {
       var cellCnt=0;
-      data[0]['firstCell']=function(id) {
+      var rdata=data[0];
+      if(jQuery.type(rdata)==="string") {
+         rdata=JSON.parse(rdata);
+      }
+      rdata['firstCell']=function(id) {
          cellCnt=0;
          return '';
       };
-      data[0]['nextCell']=function(id,i,j) {
+      rdata['nextCell']=function(id,i,j) {
          cellCnt+=1;
          return cellCnt;
       };
       var ractive = new Ractive({
-         el: 'body',
-         template: template[0],
-         data: data[0]
+         el:'body',
+         template:template[0],
+         data:rdata
       });
       
       jQuery('#sheet0').calx({
-         'autoCalculateTrigger'  : 'blur'
+         'autoCalculateTrigger':'blur'
       });
 
       var l;
@@ -30,8 +34,8 @@ jQuery(function(){
       l=jQuery('.form-group').length;
       jQuery('#r_pytania').text(l);
       jQuery('#r_punkty').text(l*100);
-
    });
+   
 });
 
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
